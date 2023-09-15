681ec43298416b7d70d02a56c7003e10b60e57d9
Alice van Wouw <alicevanwouw@gmail.com>
2023-09-16 00:09:04 +0200

    fixing index layout

--- King Price Assessment/Pages/Index.cshtml.cs
+++ King Price Assessment/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using King_Price_Assessment.Controllers;
+using King_Price_Assessment.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,12 +9,34 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
     private UserController _userController;
+    private GroupController _groupController;
 
-    public IndexModel(ILogger<IndexModel> logger, UserController userController)
-    {
-        _logger = logger;
-        _userController = userController;
-    }
+    public List<User> Users = new List<User>();
+    public List<UserGroup> Groups = new List<UserGroup>();
+
+    //public IndexModel(ILogger<IndexModel> logger, UserController userController, GroupController groupController)
+    //{
+    //    _logger = logger;
+    //    _userController = userController;
+    //    _groupController = groupController;
+    //}
+
+    //public async Task<IActionResult> OnGet()
+    //{
+    //    var actionResult = _userController.Get();
+    //    var actionResultGroup = _groupController.Get();
+    //    if (actionResult is JsonResult && actionResultGroup is JsonResult)
+    //    {
+    //        Users = (List<User>)((JsonResult)actionResult).Value;
+    //        Groups = (List<UserGroup>)((JsonResult)actionResultGroup).Value;
+
+    //        return Page();
+    //    }
+    //    else
+    //    {
+    //        return actionResult;          
+    //    }
+    //}
 
     public void OnGet()
     {
